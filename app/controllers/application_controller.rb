9515f174223aad534fe89b73ac429f300b99753b
umeki-special <manmosukowai@gmail.com>
2024-07-22 14:48:33 +0900

    2~ヘッダーの表示と、「こんにちは、〇〇さん」の表示

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,2 @@
 class ApplicationController < ActionController::Base
-  # ユーザー登録時に許可するパラメータを指定
-  before_action :configure_permitted_parameters, if: :devise_controller?
-
-  protected
-
-  def configure_permitted_parameters
-    # sign_up時に許可するパラメータを追加
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :age, :gender, :profile, :occupation, :position])
-    
-    # account_update時に許可するパラメータを追加
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :age, :gender])
-  end
 end
